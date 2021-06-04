950ce23d94e6f2b0ca30d8a7671d0441f537660b
Kadeluxe <63540878+Kadeluxe@users.noreply.github.com>
2021-06-05 00:01:01 +0300

    Initial commit

---
+++ src/components/new-file.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from "vue";
+
+interface ISampleProp {
+  field1: number;
+  field2: boolean;
+}
+
+export default defineComponent({
+  props: {
+    sample: Object as PropType<ISampleProp>
+  },
+  setup(props, ctx) {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+
+</style>
